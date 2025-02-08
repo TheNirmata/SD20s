749366d044aae86aa342804cacd09a40e4b0db1f
Aimee Nguyen <aimeeqnguyen@outlook.com>
2025-02-08 11:44:41 -0800

    [fix] file conflict

--- sd20s-web/src/app/page.tsx
+++ sd20s-web/src/app/page.tsx
@@ -2,11 +2,11 @@
 
 import React, { useEffect } from 'react';
 // import Playlist from '../components/music/playlist';
-import LandingBlock from '../components/blocks/landingBlock';
-import Header from '../components/header/header';
-import WaitlistBlock from '../components/blocks/waitlistBlock';
-import AboutUs from '../components/blocks/aboutUs';
-import Footer from '../components/blocks/footer';
+import LandingBlock from '../../components/blocks/landingBlock';
+import Header from '../../components/header/header';
+import WaitlistBlock from '../../components/blocks/waitlistBlock';
+import AboutUs from '../../components/blocks/aboutUs';
+import Footer from '../../components/blocks/footer';
 export default function Home() {
   useEffect(() => {
     document.title = 'SD20s';
